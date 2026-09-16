d7ee0b5625b44c685bb7465eaa5945a7215f211f
OTAVIO  PALESE FONTES <0220832611017@fatectq.local>
2026-09-16 12:19:09 -0300

    criação de componentes das opções

--- Front/src/pages/System/UserProfile.jsx
+++ Front/src/pages/System/UserProfile.jsx
@@ -1,5 +1,7 @@
 import SystemLayout from "@/layouts/SystemLayout";
 import style from "@/styles/profile.module.css"
+import UserOptions from "@/components/ui/UserOptions";
+import { Pencil } from "lucide-react";
 
 export default function UserProfile() {
     return (
@@ -9,23 +11,81 @@ export default function UserProfile() {
                 <div className={style.profileContainer}>
                     <div className={style.personalInfo}>
 
+                        <div className={style.topText}>
+
+                            <h2>Informações Pessoais</h2>
+                            <button><Pencil />  Editar Perfil</button>
+
+                        </div>
+
                         <div className={style.doador}>
 
                             <div>
                                 <img src="" alt="Foto de perfil do usuário" />
                                 <div>
-                                    <h2 id={style.username}>username</h2>
-                                    <p id={style.tipoSanguineo}>tipoSanguineo</p>
-                                    <p id={style.apto}>doadorApto</p>
+                                    <h1 className={style.username}>Otávio Palese</h1>
+
+                                    <div>
+                                        <p className={style.tipoSanguineo}>O+</p>
+                                        <p className={style.apto}>Doador Apto</p>
+                                    </div>
                                 </div>
                             </div>
 
-                            <span>Editar Perfil</span>
                         </div>
+
+
 
                         <div style={{ width: '100%', height: '1px', background: '#94A3B8' }}></div>
 
+                        <div className={style.dataFlexContainer}>
 
+                            <div className={style.indContainer}>
+                                <h1 className={style.dataTitle}>
+                                    email
+                                </h1>
+                                <h2 className={style.infoData}>
+                                    otavio@gmail.com
+                                </h2>
+                            </div>
+
+                             <div className={style.indContainer}>
+                                <h1 className={style.dataTitle}>
+                                    email
+                                </h1>
+                                <h2 className={style.infoData}>
+                                    otavio@gmail.com
+                                </h2>
+                            </div>
+
+                             <div className={style.indContainer}>
+                                <h1 className={style.dataTitle}>
+                                    email
+                                </h1>
+                                <h2 className={style.infoData}>
+                                    otavio@gmail.com
+                                </h2>
+                            </div>
+
+                             <div className={style.indContainer}>
+                                <h1 className={style.dataTitle}>
+                                    TIPO SANGUÍNEO
+                                </h1>
+                                <h2 className={style.infoData}>
+                                    O +
+                                </h2>
+                            </div>
+
+                             <div className={style.indContainer}>
+                                <h1 className={style.dataTitle}>
+                                    Endereço
+                                </h1>
+                                <h2 className={style.infoData}>
+                                    R. Oliveira Pastos, 289 - Cohab4, Pelourinho - MG - 14876-213 
+                                </h2>
+                            </div>
+
+                        </div>
                     </div>
 
                     <div className={style.security}>
@@ -33,9 +93,7 @@ export default function UserProfile() {
                     </div>
                 </div>
 
-                <div className={style.options}>
-
-                </div>
+                <UserOptions />
 
             </div>
         </SystemLayout>
